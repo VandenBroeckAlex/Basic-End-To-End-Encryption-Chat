1df5ed3ceeaf0156e69e39b1b8e1c955a008b3e0
Alex VdB <alexvdbr6@gmail.com>
2025-09-22 16:51:32 +0200

    foreign key and seeder

---
+++ stamp_back/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using stamp_back.Models;
+
+namespace stamp_back.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) 
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Chat> Chats { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<UserChat> UserChats { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<UserChat>()
+                .HasKey(pc => new { pc.UserId, pc.ChatId });
+        }   
+    }
+}
---
+++ stamp_back/Seeder/Seed.cs
@@ -0,0 +1,39 @@
+﻿using stamp_back.Data;
+using stamp_back.Models;
+using System.Diagnostics.Metrics;
+
+namespace stamp_back.Seeder
+{
+    public class Seed
+    {
+        private readonly DataContext dataContext;
+        public Seed(DataContext context)
+        {
+            this.dataContext = context;
+        }
+        public void SeedDataContext()
+        {
+            if (!dataContext.Users.Any())
+            {
+                var users = new List<User>()
+                {
+                    new User() { UserName = "Test", Email = "Test@test.com", Password = "Password123" },
+                    new User() { UserName = "Alice", Email = "alice@example.com", Password = "Password123" },
+                    new User() { UserName = "Bob", Email = "bob@example.com", Password = "Password123" },
+                    new User() { UserName = "Charlie", Email = "charlie@example.com", Password = "Password123" },
+                    new User() { UserName = "Diana", Email = "diana@example.com", Password = "Password123" },
+                    new User() { UserName = "Eve", Email = "eve@example.com", Password = "Password123" },
+                    new User() { UserName = "Frank", Email = "frank@example.com", Password = "Password123" },
+                    new User() { UserName = "Grace", Email = "grace@example.com", Password = "Password123" },
+                    new User() { UserName = "Heidi", Email = "heidi@example.com", Password = "Password123" },
+                    new User() { UserName = "Ivan", Email = "ivan@example.com", Password = "Password123" },
+                    new User() { UserName = "Judy", Email = "judy@example.com", Password = "Password123" }
+                };
+
+                dataContext.Users.AddRange(users);
+                dataContext.SaveChanges();
+            }
+        }
+    }
+}
+
