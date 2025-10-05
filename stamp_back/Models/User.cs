3ed368143e766362b42b1758ed071a3e0b33d423
Alex VdB <alexvdbr6@gmail.com>
2025-10-05 16:58:10 +0200

    refresh token beggening

--- stamp_back/Models/User.cs
+++ stamp_back/Models/User.cs
@@ -10,8 +10,8 @@ namespace stamp_back.Models
         [JsonIgnore] public string Password { get; set; }
 
         //public string icon_url { get; set; }
-
-
+        public string? RefreshToken {get; set;}
+        public DateTime? RefreshTokenExpiryTime { get; set; }
         // Navigation property to messages
         public ICollection<Message> Messages { get; set; }
 
